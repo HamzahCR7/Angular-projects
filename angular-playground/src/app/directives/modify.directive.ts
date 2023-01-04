f0262390d14786ae3bba360e6fbf0b098cb946ff
hamzacr7 <hamzahaslam523@gmail.com>
2023-01-04 14:31:44 +0530

    custom-directives_01

--- angular-playground/src/app/directives/modify.directive.ts
+++ angular-playground/src/app/directives/modify.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener,Input } from '@angular/core';
 
 @Directive({
-  selector: '[appModify]'
+  selector: '[appModify]',
 })
 export class ModifyDirective {
-
-  constructor() { }
-
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('green');
+  }
+  constructor(private el: ElementRef) {
+    // el.nativeElement.style.color="red";   using ElementRef directy
+    // el.nativeElement.innerHTML ="hamzah"
+  }
+  highlight(color: string) {
+    this.el.nativeElement.style.color = color;//handling user events
+  }
 }
