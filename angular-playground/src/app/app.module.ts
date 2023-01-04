e76087a7320c31ff455a5bd17abeec615c657b3b
hamzacr7 <hamzahaslam523@gmail.com>
2023-01-04 13:39:45 +0530

    created comp,service,directives for the first time

--- angular-playground/src/app/app.module.ts
+++ angular-playground/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './components/test/test.component';
+import { ModifyDirective } from './directives/modify.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    ModifyDirective
   ],
   imports: [
     BrowserModule,
