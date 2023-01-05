ec7e0c781aae7ccba14111ba0f9d47af2a8ea226
hamzacr7 <hamzahaslam523@gmail.com>
2023-01-05 22:50:10 +0530

    concept of directives and custom directives with hostbinding

--- angular-playground/src/app/components/test/test.component.ts
+++ angular-playground/src/app/components/test/test.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-
+boxData:any="";
+message:string=""
   constructor() { }
 
   ngOnInit(): void {
+  }
+  handleAction(e:any){
+this.message=e
   }
 
 }
